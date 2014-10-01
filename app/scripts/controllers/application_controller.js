5497cddf94be3807735b3c549176ef8539526b7b
Jun-Hee Lee <flyingmt@gmail.com>
2014-09-30 18:50:13 -0700

    Init.

---
+++ app/scripts/controllers/application_controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by flyingmt on 2014. 9. 30..
+ */
+
+Webmaptest.ApplicationController = Em.ObjectController.extend({
+    searchTerms: '',
+    applicationName: 'Perseus Web Map Viewer'
+});
