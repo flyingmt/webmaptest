57825e19ac1fc2c2d766970b73a8767813d0a7d9
Jun-Hee Lee <flyingmt@gmail.com>
2014-09-30 19:37:20 -0700

    Init.

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -2,6 +2,12 @@ Webmaptest.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
     model: function () {
+
+        var url = "";
+
+
+
+
         return ['red', 'yellow', 'blue'];
     }
 });
